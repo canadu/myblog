4ee7145113c510c84a490e8bd529a77c6a041a83
bikke777 <canacana2@gmail.com>
2023-01-27 17:31:49 +0900

    bootstrapに修正

--- views/admin/view_posts.php
+++ views/admin/view_posts.php
@@ -13,7 +13,7 @@
               while ($fetch_post = current($view_posts)) {
               ?>
 
-                <div class="card" style="width:15rem;">
+                <div class="card" style="width:16rem;">
 
                   <?php if ($fetch_post['image'] != '') : ?>
                     <img src="../../uploaded_img/<?php echo $fetch_post['image']; ?>" class="card-img-top" alt="<?= $fetch_post['title']; ?>の画像">
@@ -21,32 +21,29 @@
                   <div class="card-body">
 
                     <form method="POST">
-
                       <input type="hidden" name="post_id" value="<?= $fetch_post['post_id']; ?>">
-                      <div class="status" style="background-color:<?php if ($fetch_post['status'] == 'active') {
-                                                                    echo '#FFC107';
-                                                                  } else {
-                                                                    echo '#6C757D';
-                                                                  }; ?>;"><?= $fetch_post['status'] == 'active' ? '公開' : '非公開'; ?></div>
-                      <h5 class="card-title"><?= $fetch_post['title']; ?></h5>
-                      <p class="card-text"><?php echo $fetch_post['content']; ?></p>
 
-                      <div class="icons">
-                        <div class="likes"><i class="fas fa-heart"></i><span><?= $fetch_post['total_post_likes']['total']; ?></span></div>
-                        <div class="comments"><i class="fas fa-comments"></i><span><?= $fetch_post['total_post_comments']['total']; ?></span></div>
-                      </div>
+                      <?php if ($fetch_post['status'] == 'active') : ?>
+                        <span class="badge badge-danger">公開</span>
+                      <?php else : ?>
+                        <span class="badge badge-secondary">非公開</span>
+                      <?php endif; ?>
 
-
-
+                      <h6 class="card-subtitle mb-2 text-muted"><?= $fetch_post['title']; ?></h6>
+                      <p class="card-text mb-2"><?php echo $fetch_post['content']; ?></p>
                     </form>
 
+                    <div class="d-flex d-flex justify-content-between">
+                      <div><i class="fas fa-heart"></i><span> <?= $fetch_post['total_post_likes']['total']; ?></span></div>
+                      <div><i class="fas fa-comments"></i><span> <?= $fetch_post['total_post_comments']['total']; ?></span></div>
+                    </div>
 
                   </div>
                   <div class="card-footer">
                     <div class="text-right">
                       <a href="<?= $base_url; ?>/admin/edit_post/<?= $fetch_post['post_id']; ?>" class="btn btn-info btn-sm"><i class="fa-regular fa-pen-to-square"></i> 編集</a>
                       <button type="submit" name="delete" class="btn btn-danger btn-sm" onclick="return confirm('この投稿を削除しますか？');"><i class="fa-solid fa-trash-can"></i> 削除</button>
-                      <a href="<?= $base_url; ?>/admin/read_post/<?= $fetch_post['post_id']; ?>" class="btn btn-outline-info"></a>
+                      <a href="<?= $base_url; ?>/admin/read_post/<?= $fetch_post['post_id']; ?>" class="btn btn-outline-info btn-sm"><i class="fa-solid fa-circle-info"></i> 詳細</a>
                     </div>
                   </div>
                 </div>
